0b8a082ead13fa23e518b30efc881d16c97f071c
Rivendor <38053741+Rivendor@users.noreply.github.com>
2018-06-12 14:04:03 -0300

    segundo commit

--- crud.cpp
+++ crud.cpp
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #define TAMANHO_MAXIMO 100
 
+int tamanho = 0;
+int escolha, i, j, matri, *p;
+
 struct formato_aluno{
 	int matricula;
 	char nome[30];
@@ -9,6 +12,8 @@ struct formato_aluno{
 	float av2;
 	float media;
 };
+
+formato_aluno aluno[TAMANHO_MAXIMO];
 
 float calcula_media (float av1, float av2){
 	float media = ((av1 * 3) + (av2 * 4)) / 7;
@@ -19,93 +24,134 @@ void formato_linha(int matricula, char *nome, float av1, float av2, float media)
 	printf ("\nMatricula: %d\tNome: %s\tAV1: %.1f\tAV2: %.1f\tMedia: %.1f\n", matricula, nome, av1, av2, media);
 }
 
+void adiciona_aluno (int posicao){
+		
+		printf ("\nMatricula: ");
+		scanf ("%d", &aluno[posicao].matricula);
+		printf ("\nNome: ");
+		scanf (" %s[a-zA-Z ]", &aluno[posicao].nome);
+		fflush (stdin);
+		printf ("\nNota AV1: ");
+		scanf ("%f", &aluno[posicao].av1);
+		if (aluno[posicao].av1 < 0 || aluno[posicao].av1 > 10){
+			do{
+				printf ("\nValor Invalido!\nNota AV1: ");
+				scanf ("%f", &aluno[posicao].av1);
+			}while (aluno[posicao].av1 < 0 || aluno[posicao].av1 > 10);
+		}
+		printf ("\nNota AV2: ");
+		scanf ("%f", &aluno[posicao].av2);
+		if (aluno[posicao].av2 < 0 || aluno[posicao].av2 > 10){
+			do{
+				printf ("\nValor Invalido!\nNota AV2: ");
+				scanf ("%f", &aluno[posicao].av2);
+			}while (aluno[posicao].av2 < 0 || aluno[posicao].av2 > 10);
+		}
+		printf ("\n");
+		aluno[posicao].media = calcula_media(aluno[posicao].av1, aluno[posicao].av2);
+		
+}
+
+int consulta_aluno(char *msg){
+	if (tamanho == 0)
+		printf ("Nao ha alunos cadastrados!\n");
+	else{
+		printf (msg);
+		scanf ("%d", &matri);
+		for (i = 0; i < tamanho; i++){
+			if (matri == aluno[i].matricula){
+				printf ("\nPosicao: %d\tNome: %s\tAV1: %.1f\tAV2: %.1f\tMedia: %.1f\n", i, aluno[i].nome, aluno[i].av1, aluno[i].av2, aluno[i].media);
+				break;
+			}
+			else{
+				printf ("\nNumero de matricula invalida!");
+			}
+		}
+		printf ("\n");
+		return i;
+	}
+}
+
+void altera_aluno(){
+	if (tamanho == 0)
+		printf ("Nao ha alunos cadastrados!\n");
+	else{
+		consulta_aluno("Para alterar um aluno, entra com a matricula: ");
+		adiciona_aluno(i);
+		printf ("\n");
+	}
+}
+
+void exclui_aluno(){
+	if (tamanho == 0)
+		printf ("Nao ha alunos cadastrados!\n");
+	else{
+		int del;
+		del = consulta_aluno("Para excluir um aluno, entra com a matricula: ");
+		for (i = del; i < tamanho; i++){
+			aluno[i] = aluno[i+1];
+		}
+		tamanho--;
+		printf ("\n");
+	}
+}
+
+void lista_aluno(){
+	if (tamanho == 0)
+		printf ("Nao ha alunos cadastrados!\n");
+	else{
+		for (i = 0; i < tamanho; i++){
+			formato_linha (aluno[i].matricula, aluno[i].nome, aluno[i].av1, aluno[i].av2, aluno[i].media);
+		}
+		printf ("\n");
+	}
+}
+
 void menu (){
-	printf ("|Menu principal|\n");
-	printf ("1 - Adicionar\n2 - Consultar\n3 - Alterar\n4 - Excluir\n5 - Listar\n0 - Sair\n");
+	
+	
+	do{
+		printf ("|Menu principal|\n");
+		printf ("1 - Adicionar\n2 - Consultar\n3 - Alterar\n4 - Excluir\n5 - Listar\n0 - Sair\n");
+		scanf ("%d", &escolha);
+		if (escolha < 0 || escolha > 5){
+			do{
+				printf ("\nOperacao invalida!\n");
+				printf ("|Menu principal|\n");
+				printf ("1 - Adicionar\n2 - Consultar\n3 - Alterar\n4 - Excluir\n5 - Listar\n0 - Sair\n");
+				scanf ("%d", &escolha);
+			}while (escolha < 0 || escolha > 5);
+		}
+		
+		
+		// Adicionar;
+		if (escolha == 1){
+			tamanho++;
+			adiciona_aluno(tamanho-1);
+		}
+		// Consultar;
+		else if (escolha == 2){
+			consulta_aluno("Para consultar um aluno, entra com a matricula: ");
+		}
+		// Alterar;
+		else if (escolha == 3){
+			altera_aluno();
+		}
+		// Excluir;
+		else if (escolha == 4){
+			exclui_aluno();
+		}
+		// Listagem;
+		else if (escolha == 5){
+			lista_aluno();
+		}
+	}while (escolha != 0);
+	
 }
 
 main (){
-	// Adicionar; Consultar; Alterar; Excluir; Listagem
-	int escolha, i, j, tamanho, matri;
-	formato_aluno aluno[TAMANHO_MAXIMO];
-	do{
-		
-		menu();
-		scanf ("%d", &escolha);
-		
-		if (escolha == 1){
-			printf ("\nQuantidade de alunos: ");
-			scanf ("%d", &tamanho);
-			for (i = 0; i < tamanho; i++){
-				printf ("\nMatricula: ");
-				scanf ("%d", &aluno[i].matricula);
-				printf ("\nNome: ");
-				scanf (" %s[a-zA-Z ]", &aluno[i].nome);
-				fflush (stdin);
-				printf ("\nNota AV1: ");
-				scanf ("%f", &aluno[i].av1);
-				printf ("\nNota AV2: ");
-				scanf ("%f", &aluno[i].av2);
-				printf ("\n");
-				aluno[i].media = calcula_media(aluno[i].av1, aluno[i].av2);
-			}
-		}
-		else if (escolha == 2){
-			printf ("Para consultar um aluno, entra com a matricula: ");
-			scanf ("%d", &matri);
-			for (i = 0; i < tamanho; i++){
-				if (matri == aluno[i].matricula){
-					printf ("\nPosicao: %d\tNome: %s\tAV1: %.1f\tAV2: %.1f\tMedia: %.1f\n", i, aluno[i].nome, aluno[i].av1, aluno[i].av2, aluno[i].media);
-				}
-			}
-			printf ("\n");
-		}
-		else if (escolha == 3){
-			printf ("Escolha a posicao do aluno a ser alterado:\n");
-			for (i = 0; i < tamanho; i++){
-				printf ("\nPosicao: %d", i);
-				formato_linha (aluno[i].matricula, aluno[i].nome, aluno[i].av1, aluno[i].av2, aluno[i].media);
-			}
-			scanf ("%d", &j);
-			printf ("\nMatricula: ");
-			scanf ("%d", &aluno[j].matricula);
-			printf ("\nNome: ");
-			scanf (" %s[a-zA-Z ]", &aluno[j].nome);
-			fflush (stdin);
-			printf ("\nNota AV1: ");
-			scanf ("%f", &aluno[j].av1);
-			printf ("\nNota AV2: ");
-			scanf ("%f", &aluno[j].av2);
-			printf ("\n");
-			aluno[j].media = calcula_media(aluno[j].av1, aluno[j].av2);
-			printf ("\n");
-		}
-		else if (escolha == 4){
-			printf ("\nEscolha a posicao do aluno a ser deletado:\n");
-			for (i = 0; i < tamanho; i++){
-				printf ("\nPosicao: %d", i);
-				formato_linha (aluno[i].matricula, aluno[i].nome, aluno[i].av1, aluno[i].av2, aluno[i].media);
-			}
-			scanf ("%d", &j);
-			for (i = j; i < tamanho; i++){
-				aluno[i] = aluno[i+1];
-			}
-			tamanho--;
-			printf ("\n");
-		}
-		else if (escolha == 5){
-			for (i = 0; i < tamanho; i++){
-				formato_linha (aluno[i].matricula, aluno[i].nome, aluno[i].av1, aluno[i].av2, aluno[i].media);
-			}
-			printf ("\n");
-		}
-		
-		
-		
 	
-	
-	
-	}while (escolha != 0);
+	menu();
 	
 	system ("PAUSE");
 	return 0;
